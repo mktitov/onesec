3b00a938a6a09515da2efa40c9887ed5f2b2b66e
mikhail1207 <mikhail1207@b25b9702-9146-11de-a556-4d993b87afc4>
2011-09-09 13:05:23 +0000

    - added SendDataAction for conversation scenario - added possibility to do something on READY_TO_COMMUTATE in queue call cycle in the conversation scenario

---
+++ onesec-raven/src/main/java/org/onesec/raven/ivr/actions/SendDataActionNode.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2011 Mikhail Titov.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.onesec.raven.ivr.actions;
+
+import org.onesec.raven.ivr.IvrAction;
+import org.onesec.raven.ivr.IvrActionNode;
+import org.raven.annotations.Parameter;
+import org.raven.ds.DataConsumer;
+import org.raven.ds.DataContext;
+import org.raven.ds.DataSource;
+import org.raven.expr.impl.BindingSupportImpl;
+import org.raven.expr.impl.ScriptAttributeValueHandlerFactory;
+import org.raven.tree.NodeAttribute;
+import org.raven.tree.impl.BaseNode;
+
+import javax.script.Bindings;
+import java.util.Collection;
+import org.onesec.raven.ivr.IvrConversationScenario;
+import org.raven.annotations.NodeClass;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+@NodeClass(parentNode=IvrConversationScenario.class)
+public class SendDataActionNode extends BaseNode implements IvrActionNode, DataSource
+{
+    @Parameter(valueHandlerType=ScriptAttributeValueHandlerFactory.TYPE)
+    private Object expression;
+
+    private BindingSupportImpl bindingSupport;
+
+    @Override
+    protected void initFields() {
+        super.initFields();
+        bindingSupport = new BindingSupportImpl();
+    }
+
+    public IvrAction createAction() {
+        return new SendDataAction(bindingSupport, this);
+    }
+
+    public boolean getDataImmediate(DataConsumer dataConsumer, DataContext context) {
+        throw new UnsupportedOperationException("Pull mode not supported");
+    }
+
+    public Collection<NodeAttribute> generateAttributes() {
+        return null;
+    }
+
+    @Override
+    public void formExpressionBindings(Bindings bindings)
+    {
+        super.formExpressionBindings(bindings);
+        bindingSupport.addTo(bindings);
+    }
+
+    public Object getExpression() {
+        return expression;
+    }
+
+    public void setExpression(Object expression) {
+        this.expression = expression;
+    }
+}
---
+++ onesec-raven/src/main/java/org/onesec/raven/ivr/impl/IvrUtils.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2011 Mikhail Titov.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.onesec.raven.ivr.impl;
+
+import java.util.concurrent.TimeUnit;
+import org.onesec.raven.ivr.AudioStream;
+import org.onesec.raven.ivr.InputStreamSource;
+import org.onesec.raven.ivr.IvrEndpointConversation;
+import org.onesec.raven.ivr.actions.AsyncAction;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+public class IvrUtils
+{
+    private IvrUtils()     {
+    }
+
+    public static void playAudioInAction(AsyncAction action, IvrEndpointConversation conversation
+            , InputStreamSource audio)
+        throws InterruptedException
+    {
+        AudioStream stream = conversation.getAudioStream();
+        if (stream!=null){
+            stream.addSource(audio);
+            Thread.sleep(200);
+            while (!action.hasCancelRequest() && stream.isPlaying())
+                TimeUnit.MILLISECONDS.sleep(100);
+        }
+    }
+}
