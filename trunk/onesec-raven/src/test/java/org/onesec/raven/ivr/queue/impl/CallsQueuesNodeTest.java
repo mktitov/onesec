64bf96a1bb51302bfb58262d68b65648d9fcf611
mikhail1207@gmail.com <mikhail1207@gmail.com@b25b9702-9146-11de-a556-4d993b87afc4>
2012-03-22 14:27:36 +0000

    fixed a bug

--- trunk/onesec-raven/src/test/java/org/onesec/raven/ivr/queue/impl/CallsQueuesNodeTest.java
+++ trunk/onesec-raven/src/test/java/org/onesec/raven/ivr/queue/impl/CallsQueuesNodeTest.java
@@ -303,7 +303,8 @@ public class CallsQueuesNodeTest extends OnesecRavenTestCase
         provider.setFromNumber(88013);
         provider.setToNumber(88049);
         provider.setHost("10.16.15.1");
-        provider.setPassword("cti_user1");
+        provider.setPassword(privateProperties.getProperty("cti_user1"));
+        System.out.println("\n!!! private properties: "+privateProperties.getProperty("cti_user1")+"\n");
         provider.setUser("cti_user1");
         assertTrue(provider.start());
 
@@ -332,7 +333,7 @@ public class CallsQueuesNodeTest extends OnesecRavenTestCase
         IvrConversationScenarioNode abonentScenario = createAbonentScenario();
 //        createEndpoint(tree.getRootNode(), executor, manager, abonentScenario, "88013");
         createMultichannelEndpoint(executor, manager, abonentScenario);
-        createEndpoint(pool, executor, manager, null, "88013");
+//        createEndpoint(pool, executor, manager, null, "88013");
         createEndpoint(pool, executor, manager, null, "88014");
         
         assertTrue(pool.start());
@@ -378,8 +379,8 @@ public class CallsQueuesNodeTest extends OnesecRavenTestCase
         CallsQueueOperatorNode operator = new CallsQueueOperatorNode();
         operator.setName("Titov MK");
         queues.getOperatorsNode().addAndSaveChildren(operator);
-//        operator.setPhoneNumbers("88027");
-        operator.setPhoneNumbers("089128672947");
+        operator.setPhoneNumbers("88027");
+//        operator.setPhoneNumbers("089128672947");
 //        operator.setPhoneNumbers("88027,089128672947");
         operator.setParallelCallAfter(5);
         operator.setEndpointPool(pool);
