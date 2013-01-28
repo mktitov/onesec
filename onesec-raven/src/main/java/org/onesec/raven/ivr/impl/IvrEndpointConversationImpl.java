dda8f1810280b6f188345b68964d1aa3872716bc
mikhail1207 <mikhail1207@gmail.com>
2013-01-28 14:06:10 +0000

    some improvements

--- onesec-raven/src/main/java/org/onesec/raven/ivr/impl/IvrEndpointConversationImpl.java
+++ onesec-raven/src/main/java/org/onesec/raven/ivr/impl/IvrEndpointConversationImpl.java
@@ -118,6 +118,7 @@ public class IvrEndpointConversationImpl implements IvrEndpointConversation
 
         state = new IvrEndpointConversationStateImpl(this);
         state.setState(INVALID);
+//        call.getConnections()[0].getTerminalConnections()[0].
 //        stateListenersCoordinator.addListenersToState(state, IvrEndpointConversationState.class);
     }
 
